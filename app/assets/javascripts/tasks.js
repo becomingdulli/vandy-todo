d549e62a0d4803195c2037c4270fd4ed1f658bbf
Jason <becomingdulli@gmail.com>
2019-06-14 21:31:19 +0000

    Polishing the design

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,32 +1,19 @@
 $(function() {
-
-  $('#new-form').submit(function(event) {
-    event.preventDefault();
-
-    let task = {
-      task: {
-        title:  $('.new-todo').val()
-      }
-    };
-
-    $.post("/tasks", task, (data) => {
-      $('.todo-list').append(taskHtml(data));
-    });
-  });
-
   // The taskHtml method takes in a JavaScript representation
   // of the task and produces an HTML representation using
   // <li> tags
   function taskHtml(task) {
-    let checkedStatus = task.done ? "checked" : "";
-    return (`
-      <li>
-        <div class="view">
-          <input class="toggle" type="checkbox" data-id="${task.id}" ${checkedStatus} />
-          <label>${task.title}</label>
-        </div>
-      </li>
-    `);
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
+
+    return liElement;
   }
 
   // toggleTask takes in an HTML representation of the
@@ -34,22 +21,53 @@ $(function() {
   // the toggle checkbox and  performs an API request to toggle
   // the value of the `done` field
   function toggleTask(e) {
-    $.ajax(`/tasks/${$(e.target).data("id")}`, {
-      method: "PUT",
-      data: {
-        task: {
-          done: Boolean($(e.target).is(':checked'))
-        }
+    var itemId = $(e.target).data("id");
+
+    var doneValue = Boolean($(e.target).is(':checked'));
+
+    $.post("/tasks/" + itemId, {
+      _method: "PUT",
+      task: {
+        done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+
+    } );
   }
 
-  $.get("/tasks").success((data) => {
-    for (var i = 0; i < data.length; i++) {
-      $('.todo-list').append(taskHtml(data[i]));
-    }
+  $.get("/tasks").success( function( data ) {
+    var htmlString = "";
+
+    $.each(data, function(index,  task) {
+      htmlString += taskHtml(task);
+    });
+    var ulTodos = $('.todo-list');
+    ulTodos.html(htmlString);
 
     $('.toggle').change(toggleTask);
 
   });
+
+
+  $('#new-form').submit(function(event) {
+    event.preventDefault();
+    var textbox = $('.new-todo');
+    var payload = {
+      task: {
+        title: textbox.val()
+      }
+    };
+    $.post("/tasks", payload).success(function(data) {
+      var htmlString = taskHtml(data);
+      var ulTodos = $('.todo-list');
+      ulTodos.append(htmlString);
+      $('.toggle').click(toggleTask);
+      $('.new-todo').val('');
+    });
+  });
+
 });
